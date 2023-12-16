b2df43c5f68ea73d2389fbd77622f3b9d03e2127
sadufo <maxzaplitnyy@gmail.com>
2023-12-16 15:56:36 +0200

    slider touch event

--- main.js
+++ main.js
@@ -22,46 +22,70 @@ btn.addEventListener("click", openItems);
 
 // aboutUs slider
 const images = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg"];
-    let currentIndex = 0;
+let currentIndex = 0;
+let startX = 0;
+let endX = 0;
 
-    function showImage(index) {
-      const sliderContent = document.querySelector(".slider-content");
-      sliderContent.style.transform = `translateX(-${index * 100}%)`;
+function showImage(index) {
+  const sliderContent = document.querySelector(".slider-content");
+  sliderContent.style.transform = `translateX(-${index * 100}%)`;
 
-      const paginationDots = document.querySelectorAll(".pagination .dot");
-      paginationDots.forEach((dot, i) => {
-        dot.classList.toggle("active", i === index);
-      });
+  const paginationDots = document.querySelectorAll(".pagination .dot");
+  paginationDots.forEach((dot, i) => {
+    dot.classList.toggle("active", i === index);
+  });
+}
+
+function changeImage() {
+  currentIndex++;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  showImage(currentIndex);
+}
+
+function handleTouchStart(event) {
+  startX = event.touches[0].clientX;
+}
+
+function handleTouchEnd(event) {
+  endX = event.changedTouches[0].clientX;
+  if (startX - endX > 50) {
+    currentIndex++;
+    if (currentIndex >= images.length) {
+      currentIndex = 0;
     }
+  } else if (endX - startX > 50) {
+    currentIndex--;
+    if (currentIndex < 0) {
+      currentIndex = images.length - 1;
+    }
+  }
+  showImage(currentIndex);
+}
 
-    function changeImage() {
-      currentIndex++;
-      if (currentIndex >= images.length) {
-        currentIndex = 0;
-      }
+function initSlider() {
+  const sliderContent = document.querySelector(".slider-content");
+  const pagination = document.querySelector(".pagination");
+
+  for (let i = 0; i < images.length; i++) {
+    const dot = document.createElement("span");
+    dot.className = "dot";
+    dot.addEventListener("click", () => {
+      currentIndex = i;
       showImage(currentIndex);
-    }
+    });
+    pagination.appendChild(dot);
+  }
 
-    function initSlider() {
-      const sliderContent = document.querySelector(".slider-content");
-      const pagination = document.querySelector(".pagination");
+  showImage(currentIndex);
+  setInterval(changeImage, 10000);
 
-      // Создаем элементы пагинации на основе количества изображений
-      for (let i = 0; i < images.length; i++) {
-        const dot = document.createElement("span");
-        dot.className = "dot";
-        dot.addEventListener("click", () => {
-          currentIndex = i;
-          showImage(currentIndex);
-        });
-        pagination.appendChild(dot);
-      }
+  sliderContent.addEventListener("touchstart", handleTouchStart);
+  sliderContent.addEventListener("touchend", handleTouchEnd);
+}
 
-      showImage(currentIndex);
-      setInterval(changeImage, 3000); // Изменяйте интервал между сменой изображений по своему усмотрению
-    }
-
-    initSlider();
+initSlider();
 
     // burger
     var burgerMenu = document.getElementById('burger-menu');
